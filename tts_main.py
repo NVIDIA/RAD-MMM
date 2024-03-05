a8cf4909cb472c0bf85b71f4a54f50e96444cc0f
Akshit Arora <akshita@nvidia.com>
2024-03-05 22:44:19 +0000

    fixed tts main

--- tts_main.py
+++ tts_main.py
@@ -18,7 +18,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
-from pytorch_lightning.cli import LightningCLI
+from pytorch_lightning.cli import LightningCLI,ArgsType
 from tts_lightning_modules import TTSModel
 from data_modules import BaseAudioDataModule
 from jsonargparse import lazy_instance
